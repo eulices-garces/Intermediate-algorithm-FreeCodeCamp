a8d083c77578ea4384d583714b26e779fcfedeb2
eulices-garces <ulifreelancer@gmail.com>
2021-12-14 09:00:40 -0500

    Create diferencia-entre-dos-arreglos.js
    Diff Two Arrays

---
+++ diferencia-entre-dos-arreglos.js
@@ -0,0 +1,19 @@
+﻿function diffArray(arr1, arr2) {
+  const newArr = [];
+  
+  function comparando (small, large) {
+    for(let i = 0; i < large.length;i++) {
+      if (small.includes(large[i]) === false) {
+        newArr.push(large[i])
+      }
+    }
+  }  
+  
+  comparando (arr1, arr2)
+  comparando (arr2, arr1)
+
+  console.log(newArr)
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
